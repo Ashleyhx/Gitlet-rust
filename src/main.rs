7d5c515e3a2c9e94c992393ec3fd70291b844745
Ashley Huo <x2huo@uwaterloo.ca>
2024-02-25 12:01:20 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,13 @@ mod staged;
 mod serialize;
 
 fn main() {
-
     let mut repo = repo::Repo::new();
     repo.init();
     repo.add("code/test1.txt".to_string());
     repo.add("code/test2.txt".to_string());
-
-    repo.commit("initial commit".to_string());
+    repo.commit("test commit".to_string());
     repo.log();
     println!("staged are: {:?}", repo.get_staged());
-
-
-    println!("Hello, world!");
-
 }
 
 
